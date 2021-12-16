9b70c7089e12a90cc623e4177ab89ad8a3a6e1b1
Leonardo Santos <leonardo.santos@2rpnet.com>
2021-12-16 12:29:54 -0300

    pyspark 2.0

--- pyspark/pokemons_oldschool.py
+++ pyspark/pokemons_oldschool.py
@@ -4,20 +4,14 @@ spark = SparkSession \
         .builder \
         .getOrCreate()
 
-pokemonDF = spark.sql("select * from work_dataeng.pokemon_leonardo_santos")
-generationDF = spark.sql("select * from work_dataeng.generation_leonardo_santos")
+pokemonDF = spark.table('work_dataeng.pokemon_leonardo_santos')
+generationDF = spark.table('work_dataeng.generation_leonardo_santos')
 
-
-from pyspark.sql import functions as f
-generationDF.show()
 genunder2002 = generationDF.where(generationDF.date_introduced < '2002-11-21').cache()
 genunder2002.show()
-
 
 dfJoined = pokemonDF.join(generationDF, 'generation', how='inner')
 dfJoined.show(5)
 
-
-dfJoined.createOrReplaceTempView("TempView")
-spark.sql("""CREATE TABLE IF NOT EXISTS work_dataeng.pokemons_oldschool_leonardo_santos AS
-    SELECT * FROM TempView""")
+dfJoined.write.mode('overwrite').format('orc') \
+        .saveAsTable('work_dataeng.pokemons_oldschool_leonardo_santos')
